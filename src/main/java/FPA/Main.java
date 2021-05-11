a328cb80a8015f746346a3c4002d3c6f8093e867
Blaj Tyty <blajtiti16@yahoo.com>
2021-05-11 10:25:28 +0300

    Fineshed the customer log in, now they can sign in with their accounts

--- src/main/java/FPA/Main.java
+++ src/main/java/FPA/Main.java
@@ -20,8 +20,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws  WrongRoleException {
-        //initDirectory();
-        //ModeratorSevices.initDatabase();
+        initDirectory();
+        ModeratorSevices.initDatabase();
         LogInView view = new LogInView();
 
 
